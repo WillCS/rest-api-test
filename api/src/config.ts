43c7657b0290162f47e7877950f9972147070ace
WillCS <wcstibbards@gmail.com>
2019-02-08 23:33:45 +1000

    Removed default config. We don't have enough environments to justify falling back to a default no matter how cool it is.

--- api/src/config.ts
+++ api/src/config.ts
@@ -8,7 +8,6 @@ export interface Config {
 
 export default ((): Config => {
     let configFile: string;
-    let useDefault: boolean = false;
 
     switch(process.env.NODE_ENV) {
         case 'development':
@@ -18,24 +17,11 @@ export default ((): Config => {
             configFile = 'config.prod.json';
             break;
         default:
-            configFile = 'config.json';
-            useDefault = true;
-            break;
+            throw new Error('Invalid environment.');
     }
 
     const configString: string = fs.readFileSync(configDir + configFile).toString();
     const config = JSON.parse(configString);
-
-    if(!useDefault) {
-        const defaultConfigString: string = fs.readFileSync(configDir + 'config.json').toString();
-        const defaultConfig = JSON.parse(defaultConfigString);
-
-        for(const key in defaultConfig) {
-            if(!config.hasOwnProperty(key)) {
-                config[key] = defaultConfig[key];
-            }
-        }
-    }
 
     return config;
 })();
